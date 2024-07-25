64ccc506ba818eaaffc57e73bd3c470f3c230d32
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-25 09:56:24 -0400

    working on the routes

--- models/Product.js
+++ models/Product.js
@@ -24,12 +24,17 @@ Product.init(
       allowNull: false,
       defaultValue: 10,
     },
+
+    // a category has many products, so the products model has a category foreign key!
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category', // Ensure this matches the model name defined in Category.js
+        model: 'category', 
         key: 'id',
       },
+
+      // this makes sure that when a category is deleted, the products associated with it are also deleted
+      // so that there isn't any orphan data floating around in the db!
       onDelete: 'CASCADE',
       onUpdate: 'CASCADE',
     },
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,34 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // because categories can have products
+      include: [{ model: Product }]
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    //findbypk = find by primary key which is the id! req.params.id means the request params include the id
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
